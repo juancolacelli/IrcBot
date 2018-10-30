1fa9fc0cf8741bf14431062e52078f69a20cfd0c
Juan Colacelli <spam@colacelli.com>
2018-10-30 01:22:16 -0300

    Config options moved to properties file. Properties sample file added.

--- src/com/colacelli/samplebot/SampleBot.java
+++ src/com/colacelli/samplebot/SampleBot.java
@@ -11,27 +11,64 @@ import com.colacelli.samplebot.plugins.rejoinonkick.RejoinOnKickPlugin;
 import com.colacelli.samplebot.plugins.translator.TranslatorPlugin;
 import com.colacelli.samplebot.plugins.uptime.UptimePlugin;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.Properties;
 
 public class SampleBot {
+    private static final String PROPERTIES_FILE = "com/colacelli/samplebot/samplebot.properties";
+
     public static void main(String[] args) {
-
-        Server.Builder serverBuilder = new Server.Builder();
-        serverBuilder
-                .setHostname(Configurable.SERVER)
-                .setPort(Configurable.PORT)
-                .setSecure(Configurable.SECURE)
-                .setPassword(Configurable.PASSWORD);
-
-        User.Builder userBuilder = new User.Builder();
-        userBuilder
-                .setNick(Configurable.NICK)
-                .setLogin(Configurable.LOGIN);
+        Properties properties = loadProperties(PROPERTIES_FILE);
 
         IRCBot bot = new IRCBot();
 
+        addPlugins(bot, properties);
+
+        bot.connect(
+                buildServer(properties),
+                buildUser(properties)
+        );
+    }
+
+    private static Properties loadProperties(String propertiesFile) {
+        InputStream inputStream;
+        inputStream = ClassLoader.getSystemResourceAsStream(propertiesFile);
+
+        Properties properties = new Properties();
+        try {
+            properties.load(inputStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return properties;
+    }
+
+    private static User buildUser(Properties properties) {
+        User.Builder userBuilder = new User.Builder();
+        userBuilder
+                .setNick(properties.getProperty("NICK"))
+                .setLogin(properties.getProperty("LOGIN"));
+
+        return userBuilder.build();
+    }
+
+    private static Server buildServer(Properties properties) {
+        Server.Builder serverBuilder = new Server.Builder();
+        serverBuilder
+                .setHostname(properties.getProperty("SERVER"))
+                .setPort(Integer.parseInt(properties.getProperty("PORT")))
+                .setSecure(Boolean.parseBoolean(properties.getProperty("SECURE")))
+                .setPassword(properties.getProperty("PASSWORD"));
+
+        return serverBuilder.build();
+    }
+
+    private static void addPlugins(IRCBot bot, Properties properties) {
         ArrayList<Channel> channels = new ArrayList<>();
-        for (String channel : Configurable.CHANNELS) {
+        for (String channel : properties.getProperty("CHANNELS").split(",")) {
             channels.add(new Channel(channel));
         }
 
@@ -42,10 +79,5 @@ public class SampleBot {
         bot.addPlugin(new TranslatorPlugin());
 
         bot.addPlugin(new HelpPlugin());
-
-        bot.connect(
-                serverBuilder.build(),
-                userBuilder.build()
-        );
     }
 }
