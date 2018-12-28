88d133382cc7af3ff273f85c8f89f8d0bf520190
Juan Colacelli <spam@colacelli.com>
2018-12-28 12:13:17 -0300

    ApertiumResponse test added.

---
+++ src/test/kotlin/com/colacelli/ircbot/plugins/translate/apertium/ApertiumResponseTest.kt
@@ -0,0 +1,21 @@
+package com.colacelli.ircbot.plugins.translate.apertium
+
+import com.google.gson.Gson
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class ApertiumResponseTest {
+    private val json = "{\"responseData\": {\"translatedText\": \"hola\"}, \"responseDetails\": null, \"responseStatus\": 200}"
+    private val response = Gson().fromJson(json, ApertiumResponse::class.java)
+
+    @Test
+    fun getStatus() {
+        assertEquals(200, response.status)
+    }
+
+    @Test
+    fun getData() {
+        assertEquals("hola", response.data.translation)
+    }
+}
