a0fadb573a60e6754c8dc396c0ebcc75cb24d261
Juan Colacelli <spam@colacelli.com>
2019-01-02 12:43:56 -0300

    Help tests added.

---
+++ src/test/kotlin/com/colacelli/ircbot/plugins/help/HelpPluginTest.kt
@@ -0,0 +1,78 @@
+package com.colacelli.ircbot.plugins.help
+
+import com.colacelli.ircbot.IRCBot
+import com.colacelli.ircbot.base.Access
+import com.colacelli.ircbot.base.Helper
+import com.colacelli.ircbot.base.listeners.OnChannelCommandListener
+import com.colacelli.irclib.actors.Channel
+import com.colacelli.irclib.actors.User
+import com.colacelli.irclib.connection.Connection
+import com.colacelli.irclib.messages.ChannelMessage
+import com.nhaarman.mockitokotlin2.*
+import kotlinx.coroutines.runBlocking
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class HelpPluginTest {
+    private val connection = mock<Connection>()
+    private val helper = mock<Helper>()
+    private val access = mock<Access>() {
+        on { get(any()) } doReturn Access.Level.USER
+    }
+    private val bot = mock<IRCBot> {
+        on { connection } doReturn connection
+        on { helper } doReturn helper
+        on { access } doReturn access
+    }
+
+    private val plugin = HelpPlugin()
+
+    @Test
+    fun getName() {
+        assertEquals("help", plugin.name)
+    }
+
+    @Test
+    fun onLoad() {
+        plugin.onLoad(bot)
+        argumentCaptor<OnChannelCommandListener>().apply {
+            verify(bot).addListener(capture())
+
+            assertEquals(".help", firstValue.command)
+            assertEquals(Access.Level.USER, firstValue.level)
+            assertEquals(".?", firstValue.aliases!!.joinToString(""))
+        }
+    }
+
+    @Test
+    fun onUnload() {
+        plugin.onUnload(bot)
+        argumentCaptor<String>().apply {
+            verify(bot).removeListener(capture())
+            assertEquals(".help", firstValue)
+        }
+    }
+
+    @Test
+    fun commands() {
+        var listener : OnChannelCommandListener
+
+        val message = mock<ChannelMessage> {
+            on { channel } doReturn Channel("#test")
+            on { sender } doReturn User("r")
+        }
+
+        plugin.onLoad(bot)
+        argumentCaptor<OnChannelCommandListener>().apply {
+            verify(bot).addListener(capture())
+
+            listener = firstValue
+        }
+
+        runBlocking {
+            listener.onChannelCommand(connection, message, ".help", arrayOf(""))
+        }
+        verify(bot.helper).list(Access.Level.USER, "")
+    }
+}
